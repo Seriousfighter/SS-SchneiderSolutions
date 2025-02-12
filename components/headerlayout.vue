6f528cc55b9aeacd8247432cfd697cbc98ffc882
Seriousfighter <schneideralan130892@gmail.com>
2025-02-12 16:00:24 -0300

    update

---
+++ components/headerlayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>header</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <headerlayout />
+    <slot />
+    <footerlayout />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
