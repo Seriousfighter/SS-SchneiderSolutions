2bb87b7c6f8991b2c541badd687029b4ffeb973f
Seriousfighter <schneideralan130892@gmail.com>
2025-02-25 19:58:43 -0300

    Update heroSection.vue

--- equipo.guru/components/heroSection.vue
+++ equipo.guru/components/heroSection.vue
@@ -8,7 +8,7 @@
                     <p class="text-grey-shades-60">En EquipoDOTGuru, creemos en el poder transformador de las soluciones digitales. Nuestro equipo de expertos está dedicado a ayudar a empresas como la tuya a prosperar en
                     el dinámico mundo digital</p>
                 </div>
-                <div>
+                <div class="mx-auto md:mx-0">
                     <h3 class="text-white-guru mb-[15px] ">Desbloquea tu Potencial Digital Hoy</h3>
                     <BtnGreen>Comencemos</BtnGreen>
                 </div>
