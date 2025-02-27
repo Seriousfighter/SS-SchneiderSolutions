7ad4261d81793424c534e07343a03cbc6fa4f1d1
Seriousfighter <schneideralan130892@gmail.com>
2025-02-27 15:53:31 -0300

    seo

--- equipo.guru/nuxt.config.ts
+++ equipo.guru/nuxt.config.ts
@@ -4,12 +4,33 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    '@nuxt/test-utils/module'
+    '@nuxt/test-utils/module',
+    '@nuxtjs/sitemap',
   ],
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      title: 'EquipoDOTGuru - Soluciones Digitales A Medida',
+      meta: [
+        { name: 'description', content: 'En EquipoDOTGuru, creemos en el poder transformador de las soluciones digitales. Nuestro equipo de expertos está dedicado a ayudar a empresas como la tuya a prosperar en el dinámico mundo digital.' },
+        { name: 'keywords', content: 'desarrollo web, desarrollo de software, diseño web, frontend, backend, soporte técnico, equipo digital' },
+        { name: 'robots', content: 'index, follow' },
+        
+        // Open Graph / Facebook
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://equipo.guru/' },
+        { property: 'og:title', content: 'EquipoDOTGuru - Soluciones Digitales A Medida' },
+        { property: 'og:description', content: 'Soluciones digitales a medida para potenciar tu proyecto. Equipos especializados en frontend, backend, diseño y soporte.' },
+        { property: 'og:image', content: 'https://equipo.guru/og-image.jpg' },
+        
+        // Twitter
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:url', content: 'https://equipo.guru/' },
+        { name: 'twitter:title', content: 'EquipoDOTGuru - Soluciones Digitales A Medida' },
+        { name: 'twitter:description', content: 'Soluciones digitales a medida para potenciar tu proyecto. Equipos especializados en frontend, backend, diseño y soporte.' },
+        { name: 'twitter:image', content: 'https://equipo.guru/twitter-image.jpg' },
+      ],
       link: [
         {
           rel: 'preconnect',
@@ -31,5 +52,9 @@ export default defineNuxtConfig({
         },
       ],
     }
+  },
+  sitemap: {
+    siteUrl: 'https://equipo.guru',
+    exclude: ['/admin/**']
   }
 })
