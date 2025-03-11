98f9f51f21f3c0ac669fd980c5d28f55f2b83a41
Seriousfighter <schneideralan130892@gmail.com>
2025-03-11 20:05:00 -0300

    added tailwind

--- components/headerlayout.vue
+++ components/headerlayout.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <h1>header</h1>
+    <div>
+      <h1>SS-SCHNEIDER SERVICES</h1>
+    </div>
+    <div>
+      <nav>
+        <navLink :page="home">Home</navLink>
+        <navLink :page="about">About</navLink>
+        <navLink :page="services">Services</navLink>
+        <navLink :page="contact">Contact</navLink>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+// Define the page routes
+const home = "/"
+const about = "/about"
+const services = "/services"
+const contact = "/contact"
 </script>
 
 <style>
---
+++ components/navLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink :to="page"
+  class="
+  ">
+    <slot></slot>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps<{
+    page: string
+  }>()
+</script>
+
+<style>
+
+</style>
